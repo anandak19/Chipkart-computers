cf203500b400c56407039e1b6e0bba45a41d8ee9
Anandakrishnan H <anandak19h@gmail.com>
2025-01-24 12:48:17 +0530

    updated default view and looding view for product page

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,133 +9,119 @@ exports.getHome = (req, res) => {
 // get all products page
 exports.getProductsPage = async (req, res) => {
   try {
-
-
-
     //  // getting category to show category names on view
-     const categoryArray = await CategoriesSchema.aggregate([
-       {
-         $match: {
-           isListed: true,
-         },
-       },
-       {
-         $project: {
-           _id: 0,
-           id: "$_id",
-           categoryName: 1,
-         },
-       },
-     ]);
+    const categoryArray = await CategoriesSchema.aggregate([
+      {
+        $match: {
+          isListed: true,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          id: "$_id",
+          categoryName: 1,
+        },
+      },
+    ]);
     //  res.json(products)
-    res.render("user/productPage", {categoryArray});
+    res.render("user/productPage", { categoryArray });
   } catch (error) {
     console.log(error);
   }
 };
 
-
-exports.getAvailableProducts = async(req, res) => {
-
+exports.getAvailableProducts = async (req, res) => {
   try {
-    const {
-      categoryId,
-      priceOrder,
-      ratingsAbove,
-      sortBy
-    } = req.query;
-    
-    const filters = {isListed: true};
+    const { categoryId, priceOrder, ratingsAbove, sortBy } = req.query;
+
+    const filters = { isListed: true };
     const sort = {};
 
-    let {isFeatured, isNew} = req.query
-    isFeatured = isFeatured === 'true';
-    isNew = isNew === 'true';
+    let { isFeatured, isNew } = req.query;
+    isFeatured = isFeatured === "true";
+    isNew = isNew === "true";
 
-    const { page = '0' } = req.query;
+    const { page = "0" } = req.query;
     const pageNumber = parseInt(page, 10) || 0;
-    
-    let limit = 15;
+    console.log(typeof pageNumber);
+
+    let limit = 2;
     let skip = pageNumber * limit;
 
     if (isFeatured) {
-      filters.isFeatured = isFeatured
+      filters.isFeatured = isFeatured;
     }
-    
+
     // filter by category id
     if (categoryId) {
       filters.categoryId = categoryId;
     }
-// Default - sort price in ascending----------------
+    // Default - sort price in ascending----------------
     if (priceOrder) {
       sort.finalPrice = priceOrder === "desc" ? -1 : 1;
     }
-// Default -  sort my product name in asc---------------- 
-    if (sortBy === 'a-z') {
-      sort.productNameLower = 1
-    }else if(sortBy == 'z-a'){
-      sort.productNameLower = -1
+    // Default -  sort my product name in asc----------------
+    if (sortBy === "a-z") {
+      sort.productNameLower = 1;
+    } else if (sortBy == "z-a") {
+      sort.productNameLower = -1;
     }
 
     if (isNew) {
-      sort.createdAt = 1
-      limit = 10
-      skip = 0
+      sort.createdAt = 1;
+      limit = 10;
+      skip = 0;
     }
-    
-    // Need to perform lookup with review collection for this 
+
+    // Need to perform lookup with review collection for this
     // if (ratingsAbove) {
     //   filters.ratings = { $gte: Number(ratingsAbove) }
     // }
 
-
     const pipeline = [
       {
-        $match: filters, // Apply filters
+        $match: filters,
       },
       {
         $addFields: {
-          productNameLower: { $toLower: "$productName" }, // Add a lowercased product name field
+          productNameLower: { $toLower: "$productName" },
         },
       },
     ];
-    
-    // Add sorting stage only if 'sort' is defined and not empty
+
     if (sort && Object.keys(sort).length > 0) {
       pipeline.push({
-        $sort: sort, // Apply sorting
+        $sort: sort,
       });
     }
-    
+
     // Add skip and limit stages for pagination
-    pipeline.push(
-      { $skip: skip },
-      { $limit: limit }
-    );
+    pipeline.push({
+      $facet: {
+        productCount: [{ $count: "total" }],
+        paginatedResults: [{ $skip: skip }, { $limit: limit }],
+      },
+    });
 
+    const result = await ProductSchema.aggregate(pipeline);
+    const productCount = result[0].productCount[0];
+    const products = result[0].paginatedResults;
 
-
-
-
-    const products = await ProductSchema.aggregate(pipeline);
+    const total = productCount ? productCount.total : 0;
+    const hasMore = skip + products.length < total;
 
     console.log("filter is: ", filters);
-    console.log("sort is:", sort)
-    console.log("limit is:", limit)
-    console.log("skip is", skip)
+    console.log("sort is:", sort);
+    console.log("limit is:", limit);
+    console.log("skip is", skip);
 
-    res.json(products)
-    
+    res.json({ products, total, hasMore });
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-
 };
-
-
-
-
 
 exports.getAccount = (req, res) => {
   res.render("user/account");
--- public/javascripts/apiCalls/productPage.js
+++ public/javascripts/apiCalls/productPage.js
@@ -5,46 +5,66 @@ const isFeaturedCheckbox = document.getElementById("isFeatured");
 const isNewCheckbox = document.getElementById("isNew");
 const isPopularCheckbox = document.getElementById("isPopular");
 const categoryList = document.getElementById("categoryList");
-let categoryId
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const resultCount = document.getElementById("resultCount");
 
+let categoryId;
+let page = 0;
 
 // to display products with details
-const showProducts = (products) => {
+const showProducts = (products, total, hasMore) => {
   const productList = document.getElementById("productList");
-  productList.innerHTML = "";
-
-  products.forEach((product) => {
-    const productCard = `
-        <div class="product-card">
-          <div class="product-card-image">
-            <img src="${product.images[0].filepath}" alt="${product.productName}" />
-          </div>
-          <div class="product-card-details">
-            <h4>
-              <a href="#">${product.productName}</a>
-            </h4>
-            <div class="price">
-              <p class="selling-price">${product.finalPrice}</p>
-              <p class="actual-price">${product.mrp}</p>
+  if (!products || products.length === 0) {
+    productList.innerHTML = "No Products to show";
+    nextBtn.style.display = "none";
+    prevBtn.style.display = "none";
+    resultCount.innerHTML = `${total} Results`;
+  } else {
+    productList.innerHTML = "";
+    resultCount.innerHTML = `${total} Results`;
+    nextBtn.style.display = "flex";
+    prevBtn.style.display = "flex";
+    nextBtn.disabled = !hasMore;
+    prevBtn.disabled = page === 0;
+    products.forEach((product) => {
+      const imageUrl =
+        product.images && product.images.length > 0
+          ? product.images[0].filepath
+          : "path/to/default-image.jpg";
+      const productCard = `
+          <div class="product-card">
+            <div class="product-card-image">
+              <img src="${imageUrl}" alt="${product.productName || 'Default Product'}" />
+            </div>
+            <div class="product-card-details">
+              <h4>
+                <a href="#">${product.productName || 'Unnamed Product'}</a>
+              </h4>
+              <div class="price">
+            <p class="selling-price">${product.finalPrice || 'N/A'}</p>
+            <p class="actual-price">${product.mrp || 'N/A'}</p>
+              </div>
+            </div>
+            <div class="product-actions">
+              <button class="add-cart">Add Cart</button>
+              <button class="wishlist">Wishlist</button>
             </div>
           </div>
-          <div class="product-actions">
-            <button class="add-cart">Add Cart</button>
-            <button class="wishlist">Wishlist</button>
-          </div>
-        </div>
-      `;
+        `;
 
-    productList.insertAdjacentHTML("beforeend", productCard);
-  });
+      productList.insertAdjacentHTML("beforeend", productCard);
+    });
+  }
 };
 
 // on page load method
 async function fetchProducts() {
   try {
     const response = await fetch("/products/p");
-    const products = await response.json();
-    showProducts(products);
+    const { products, total, hasMore } = await response.json();
+
+    showProducts(products, total, hasMore);
   } catch (error) {
     console.error("Error fetching products:", error);
     alert("Something went wrong");
@@ -62,10 +82,10 @@ const getFilters = () => {
   if (isFeaturedCheckbox.checked) filters.isFeatured = true;
   if (isNewCheckbox.checked) filters.isNew = true;
   if (isPopularCheckbox.checked) filters.isPopular = true;
-
   if (categoryId) {
-    filters.categoryId = categoryId
+    filters.categoryId = categoryId;
   }
+  filters.page = page;
 
   return filters;
 };
@@ -75,12 +95,14 @@ const applyFilters = async () => {
   const filters = getFilters();
 
   const queryString = new URLSearchParams(filters).toString();
-  console.log(queryString)
+  console.log(queryString);
 
   try {
     const response = await fetch(`/products/p?${queryString}`);
-    const filteredProducts = await response.json();
-    showProducts(filteredProducts);
+    const { products, total, hasMore } = await response.json();
+    console.log(products);
+
+    showProducts(products, total, hasMore);
   } catch (error) {
     console.error("Error applying filters:", error);
     alert("Something went wrong while applying filters.");
@@ -90,7 +112,7 @@ const applyFilters = async () => {
 // event listeners to filters
 priceOrder.addEventListener("change", applyFilters);
 rating.addEventListener("change", applyFilters);
-sortBy.addEventListener("change", applyFilters)
+sortBy.addEventListener("change", applyFilters);
 isFeaturedCheckbox.addEventListener("change", applyFilters);
 isNewCheckbox.addEventListener("change", applyFilters);
 isPopularCheckbox.addEventListener("change", applyFilters);
@@ -101,11 +123,21 @@ categoryList.addEventListener("click", async (event) => {
     console.log("Selected Category ID:", categoryId);
 
     document.querySelectorAll("#categoryList li").forEach((li) => {
-        li.classList.remove("category-selected");
+      li.classList.remove("category-selected");
     });
 
     event.target.classList.add("category-selected");
 
-    applyFilters()
+    applyFilters();
   }
+});
+
+nextBtn.addEventListener("click", () => {
+  page++;
+  applyFilters();
+});
+
+prevBtn.addEventListener("click", () => {
+  page--;
+  applyFilters();
 });
