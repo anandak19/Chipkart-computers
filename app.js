ae94ccb5cd8f19e3fae5cc0b023fcd9aca5eaf39
Anandakrishnan H <anandak19h@gmail.com>
2026-05-14 15:24:36 +0530

    feat: add feature to perform demo login

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.disable('view cache');
+app.set('view cache', false);
+
 app.use(cookieParser());
 
 app.use((req, res, next) => {
